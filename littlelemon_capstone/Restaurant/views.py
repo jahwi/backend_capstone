bf97c4b4f335d654e7f04fb72d9f932f8642d95e
Jahwi <jawandoyanju@gmail.com>
2023-01-16 16:32:58 +0000

    add views for viewing menu items

--- littlelemon_capstone/Restaurant/views.py
+++ littlelemon_capstone/Restaurant/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import MenuSerializer, BookingSerializer
+from .models import Menu, Bookings
 
 # Create your views here.
 def index(request):
     return render(request, "index.html", {})
+
+class MenuItemsView(generics.ListCreateAPIView):
+    serializer_class = MenuSerializer
+    model = Menu
+    queryset = Menu.objects.all()
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    serializer_class = MenuSerializer
+    model = Menu
+    queryset = Menu.objects.all()

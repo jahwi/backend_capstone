9c9e519e977c067354a2381f2de1310dd56b1a20
Jahwi <jawandoyanju@gmail.com>
2023-01-16 17:08:37 +0000

    add athtoken and token-based auth support

--- littlelemon_capstone/Restaurant/urls.py
+++ littlelemon_capstone/Restaurant/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, include
 from . import views
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
 router.register(r'tables', views.BookingsViewSet)
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("api-token-auth/", obtain_auth_token),
     path("menu/", views.MenuItemsView.as_view()),
     path("menu/<int:pk>", views.SingleMenuItemView.as_view()),
     path("booking/", include(router.urls)),
